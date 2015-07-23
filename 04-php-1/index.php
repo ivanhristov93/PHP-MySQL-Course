4dc0684f7cdb9145e705d81d50b9a4a723087154
ivanhristov93 <tik9@abv.bg>
2015-07-23 21:47:57 +0300

    practice

--- 04-php-1/index.php
+++ 04-php-1/index.php
@@ -1,1 +1,36 @@
-index
+<?php 
+	$myname = "ivan";
+	echo $myname;
+	$myfamily = "hristov";
+	echo $myfamily;
+	echo $myname ." ". $myfamily;
+	
+	echo "<br />";
+	
+	$dayofmonth = 23;
+	if($dayofmonth > 25 || $dayofmonth == 23)
+	{
+		echo "Zaplatata e blizo";
+	} 
+	else 
+	{
+		echo "Zaplatata e daleche";
+	}		
+	
+	echo "<br />";
+	$a = 15;
+	$b = 13;
+	$b += $a;
+	echo $b;
+	 echo "<br />";
+	 
+	 $a = 15; 
+	 $b = 13;
+	 $a -= $b;
+	 echo $a;
+	echo "<br />";
+	 $a = 15;
+	 $b = 13;
+	 $a *= $b;
+	 echo $a;
+?>	

7a32dec538a212db28c7a8432c1c1ccf7b68da90
ivanhristov93 <tik9@abv.bg>
2015-07-23 23:47:46 +0300

    homework-01

--- 04-php-1-homework/01/php-homework-01.php
+++ 04-php-1-homework/01/php-homework-01.php
@@ -4,12 +4,12 @@
 		{
 			echo "Az sum 15";
 		}
-	if($a > 26)
+	else if ($a > 26)
 		{
 			echo "Po- golqmo ot 26";
 		}
-	if($a <= 64)
-		{	
-			echo "Po-malko ili ravno na 64";
+	else ($a <= 64);
+		}
+			echo "Po- malko ili ravno na 64";
 		}
 ?>

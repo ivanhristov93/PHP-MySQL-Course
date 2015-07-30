f6e46f451c63477ec8a736c76a41461c8c0aabab
ivanhristov93 <tik9@abv.bg>
2015-07-31 00:36:34 +0300

    06-php-2homework

---
+++ 06-php-homework/06-php-2homework.php
@@ -0,0 +1,2 @@
+<?php
+$asdArr = array("copanyname" => "Facebook", "web-site" = > "https://www.facebook.com/" ("companyname" => "Zamunda", "web-site" => "http://www.zamunda.net/" ("companyname" => "Github", "web-site" => "https://github.com" ("companyname" => "YouTube", "web-site" => "https://www.youtube.com/" ("companyname" => "ABV", "web-site" => "http://www.abv.bg/" )))));

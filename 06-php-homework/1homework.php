10b17196b2d2835b919de1ea9b158ca0a1bcf338
ivanhristov93 <tik9@abv.bg>
2015-07-31 21:50:35 +0300

    1homework

--- 06-php-homework/1homework.php
+++ 06-php-homework/1homework.php
@@ -11,9 +11,9 @@
 	}
 	unset($array[5]);
 	echo "<br />";
-	echo "<p>Ще си купя" . $array[0] . "</p>";
-	echo "<p>Ще си купя" . $array[1] . "</p>";
-	echo "<p>Ще си купя" . $array[2] . "</p>";
-	echo "<p>Ще си купя" . $array[3] . "</p>";
-	echo "<p>Ще си купя" . $array[4] . "</p>";
+	$size = count($array);
+	for($i = 0; $i < $size; $i++)
+	{
+	echo "<p>Ще си купя" . $array[$i] . "</p>";
+	}
 ?>

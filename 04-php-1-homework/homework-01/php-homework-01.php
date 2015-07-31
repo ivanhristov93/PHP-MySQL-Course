e77f7d9c95aabddce893869b231657e8fad94b07
ivanhristov93 <tik9@abv.bg>
2015-07-31 22:38:06 +0300

    gotov

--- 04-php-1-homework/homework-01/php-homework-01.php
+++ 04-php-1-homework/homework-01/php-homework-01.php
@@ -26,4 +26,31 @@
 	{
 		echo "po- malko ili ravno na 15";
 	}
+	$a = 15;
+	if($a == 15 || $a > 26)
+	{
+		echo "az sum 15";
+	}
+	else
+	{
+		echo "po- golqmo ot 26";
+	}
+	if($a <= 64)
+    {
+        echo "<span style=\"background-color:red;\">po- malko ili ravno na 64</span>";
+	}
+	echo "<br />";
+	$a = 15;
+	if($a == 15 || $a > 26)
+	{
+		echo "az sum 15";
+	}
+	else
+	{
+		echo "po- golqmo ot 26";
+	}
+	if($a <= 15)
+	{
+		echo "po- malko ili ravno na 15";
+	}
 ?>

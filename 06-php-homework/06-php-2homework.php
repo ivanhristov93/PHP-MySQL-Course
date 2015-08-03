a4f81cfca7302cbe4913320f6693c6188c317f54
mraiur <mraiur@gmail.com>
2015-08-04 01:28:49 +0300

    Note

--- 06-php-homework/06-php-2homework.php
+++ 06-php-homework/06-php-2homework.php
@@ -3,9 +3,11 @@
 			array("name" => "Zamunda", "web-site" => "http://www.zamunda.net/");
 			array("name" => "Github", "web-site" => "https://github.com");
 			array("name" => "YouTube", "web-site" => "https://www.youtube.com/");
-			array("name" => "ABV", "web-site" => "http://www.abv.bg/"); 
-		echo "<dt> Ime na kompaniqta: </dt>";
+			array("name" => "ABV", "web-site" => "http://www.abv.bg/");
+    // Виждаме само първия а не вскички.
+
+            echo "<dt> Ime na kompaniqta: </dt>";
 		echo "<dd>" . $asdArr["name"]. "</dd>";
-		
+
 		echo "<dt> Site: </dt>";
 		echo "<dd>" . $asdArr["web-site"] . "</dd>";

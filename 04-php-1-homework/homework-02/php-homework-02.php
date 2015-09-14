dc0b5b0f5a0568bb0c09910f56ba2f1b18d795ad
stamat <tik9@abv.bg>
2015-09-14 23:04:17 +0300

    asd

--- 04-php-1-homework/homework-02/php-homework-02.php
+++ 04-php-1-homework/homework-02/php-homework-02.php
@@ -1,1 +1,17 @@
-<?php
+<html>
+<head>
+<style>
+
+body {
+	background-color= "#000000";
+}
+
+.randomcolor{
+	background-color: rgb(<?=rand(0, 255)?>, <?=rand(0, 255)?>, <?=rand(0, 255)?>);
+}
+</style>
+</head>
+<body>
+	<div class="randomcolor" style="width:200px;height:200px;">
+	<h1>Аз съм космонафт</h1>
+</body>

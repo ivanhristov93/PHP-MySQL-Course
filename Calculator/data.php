39351d4af5843a31a40ff799f7a89640462e4ff7
stamat <tik9@abv.bg>
2015-09-16 22:50:29 +0300

    calculator

---
+++ Calculator/data.php
@@ -0,0 +1,8 @@
+<?php
+
+	function Calculate($num1, $mun2)
+	{
+		$first+$second = $result;
+		return $result;
+	}
+?>
---
+++ Calculator/index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+<title>Калкулатор</title>
+<link rel=StyleSheet media="screen" type="text/css" href="style.css" />
+</head>
+<body>
+	<font color="#CC0000">Calculator</font>
+		<div id="form1">
+			<form action="data.php?2" method="GET">
+				<input name="first" type="text" size="10" maxlength="9" >+ 
+				<input name="second" type="text" size="10" maxlength="9">= 
+				<input type="submit" name="submit" value="Равно на..."> 
+			</form>
+		</div>
+		<?php
+
+			$num1 = "first";
+	
+			$num2 = "second";
+	
+			$result = Calculate($num1, $num2);
+
+			function Calculate($first, $second)
+			{
+				$result=($first+$second);
+				print_r($result	);
+			}
+		?>		
+</body>
+</html>

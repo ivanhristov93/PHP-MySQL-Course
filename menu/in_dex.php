6d5edea52111613c8023e753f051e3403f47532c
stamat <tik9@abv.bg>
2015-09-16 22:42:44 +0300

    menu s html i css

---
+++ menu/in_dex.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+	<head>
+		<title>Title</title>
+		<style type="text/css">
+			#menu {
+				background: #CFCFCA;
+				border-radius: 7px;
+				padding: 15px;
+			}
+			#menu a {
+				color: #196CE8;
+				text-decoration: none;
+				font-family: Constantia;
+				word-spacing: 15px;
+				padding: 15px;
+			}
+			#menu a:hover {
+				background: #CFCFCA;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="menu">
+			<a href="page">Начало</a>
+			<a href="page">Снимки</a>
+			<a href="page">За мен</a>
+		</div>
+	</body>
+</html>
